b380998ffee0cd729a9b77e389b55e8441b5546f
mcsalah <macsalah@gmail.com>
2022-11-30 13:57:20 +0100

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,3 +1,8 @@
+resource "azurerm_network_security_group" "aks_rg" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+}
 resource "azurerm_virtual_network" "vnet1" {
   name                = "example-network"
   location            = azurerm_resource_group.aks_rg.location
