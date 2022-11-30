122b41b7703d90e19dd3129948c2dd57555e19f6
mcsalah <macsalah@gmail.com>
2022-11-30 15:49:15 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,8 +5,14 @@ terraform {
       version = "~>2.0"
     }
   }
-
+   backend "azurerm" {
+        resource_group_name  = "tfstate"
+        storage_account_name = "rgdev30112022"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
 }
+ 
 provider "azurerm" {
   features {}
 }
