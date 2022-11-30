1ec3e16b88e17b4c99e997a626c2fbef0a4f44ef
mcsalah <macsalah@gmail.com>
2022-11-30 16:28:47 +0100

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,6 +1,6 @@
 
-resource "azurerm_virtual_network" "vnet1" {
-  name                = "vnet1"
+resource "azurerm_virtual_network" "vnet10" {
+  name                = "vnet10"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "vnet1" {
 resource "azurerm_subnet" "sub1" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.aks_rg.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
+  virtual_network_name = azurerm_virtual_network.vnet10.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
